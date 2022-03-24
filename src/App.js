28833fb5ab119aa935d0fa6474a517001d11cd07
lixingyang <1433565248@qq.com>
2022-03-24 15:13:16 +0800

    调整router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 import Move from './components/Move'
 import Winner from './components/Winner'
 import Buy from './components/Buy'
+
+// import { Route, HashRouter, Switch } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom'
 
 import './App.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        卷动盘
+        <Router>
+          <ul>
+            <li>
+              <Link to="/move">Move</Link>
+            </li>
+            <li>
+              <Link to="/winner">Winner</Link>
+            </li>
+            <li>
+              <Link to="/buy">Buy</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/move" element={<Move />}></Route>
+            <Route path="/winner" element={<Winner />}></Route>
+            <Route path="/buy" element={<Buy />}></Route>
+          </Routes>
+        </Router>
       </header>
-      <ul>
+      {/* <ul>
         <li>
           <Link to="/move">Move</Link>
         </li>
@@ -34,7 +54,8 @@ function App() {
           <Route path=":winner" element={<Winner />}></Route>
           <Route path=":buy" element={<Buy />}></Route>
         </Routes>
-      </Router>
+      </Router> */}
+
     </div>
   );
 }
